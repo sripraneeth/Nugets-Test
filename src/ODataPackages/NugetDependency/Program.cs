3c228437dfc296147b5b19fc9fa345daed5549d6
Sripraneeth Nara <sripraneeth.nara@aveva.com>
2021-11-21 08:04:07 -0800

    First Test

---
+++ src/ODataPackages/NugetDependency/Program.cs
@@ -0,0 +1,23 @@
+﻿// ------------------------------------------------------------------------------------------------------------
+// <copyright company="AVEVA Software, LLC" file="Program.cs">
+//   Copyright © 2018 AVEVA Group plc and its subsidiaries. All rights reserved.
+//
+// THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+// KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+// PARTICULAR PURPOSE.
+// </copyright>
+// <summary>
+//
+// </summary>
+// ------------------------------------------------------------------------------------------------------------
+
+namespace NugetDependency
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+        }
+    }
+}
---
+++ src/ODataPackages/NugetDependency/Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿// ------------------------------------------------------------------------------------------------------------
+// <copyright company="AVEVA Software, LLC" file="AssemblyInfo.cs">
+//   Copyright © 2018 AVEVA Group plc and its subsidiaries. All rights reserved.
+//
+// THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+// KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+// PARTICULAR PURPOSE.
+// </copyright>
+// <summary>
+//
+// </summary>
+// ------------------------------------------------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a2fe198f-75c0-4a1e-b3f9-c175e781590e")]
+[assembly: AssemblyVersion("1.0.0.0")]
